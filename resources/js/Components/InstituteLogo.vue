f7ad985e08c52a1efb52d7b90d3c412983f81682
Hebert Suarez Burgos <suarezburgoshebert@gmail.com>
2026-07-01 11:27:31 -0400

    logo

--- resources/js/Components/InstituteLogo.vue
+++ resources/js/Components/InstituteLogo.vue
@@ -1,12 +1,20 @@
 <script setup>
+import { computed } from 'vue';
+import { usePage } from '@inertiajs/vue3';
+
 defineProps({
     size: { type: Number, default: 42 },
+});
+
+const logoUrl = computed(() => {
+    const sitio = usePage().props.sitio;
+    return sitio?.logo_url ?? '/images/logo.png';
 });
 </script>
 
 <template>
     <img
-        src="/images/logo.png"
+        :src="logoUrl"
         alt="Instituto San Pablo del Oriente"
         :style="{ width: size + 'px', height: size + 'px', objectFit: 'contain' }"
     />
